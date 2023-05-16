b827539f93bdcb2c9ce627749127d3f4af6fce4b
Evan Carlson <evanrc1@gmail.com>
2023-05-16 12:24:24 -0700

    fixed Api Call

--- assets/script.js
+++ assets/script.js
@@ -1,81 +1,83 @@
 ///APIKey
 var APIKey = "8abd961d0015a3cb9fcb6f613530c1d4"
 
-var city= "";
+var city = "";
 //variables declared
-var citySearch= $("#city-search");
-var searchButton= $("#searchbutton");
-var cityName= $("#cityname");
-var currentTemperature= $("#temperature");
-var currentUvIndex= $("#uv-index");
-var currentWindSpeed= $("#wind-speed");
-var currentHumidity= $("#humidity");
+var citySearch = document.querySelector("#city-search");
+var searchButton = document.querySelector("#searchbutton");
+var cityName = document.querySelector("#cityname");
+var currentTemperature = document.querySelector("#temperature");
+var currentUvIndex = document.querySelector("#uv-index");
+var currentWindSpeed = document.querySelector("#wind-speed");
+var currentHumidity = document.querySelector("#humidity");
 
 //retrieved stored cities from the local storage
-var storedCities= localStorage.getItem('Cities');
-var sCity= storedCities ? JSON.parse(storedCities) : [];
+var storedCities = localStorage.getItem('Cities');
+var sCity = storedCities ? JSON.parse(storedCities) : [];
 
 //event listener added for the search button
-searchButton.on('click' , searchCity);
+searchButton.addEventListener('click', searchCity);
 
 //Function to get weather data from the open weather api
 function getWeatherData(city) {
-    var queryURL = 
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    city +
-    "&appid=" +
-    APIKey;
-
+    var queryURL =
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&appid=" +
+        APIKey;
+    console.log(queryURL)
     //API request using the fetch()
-fetch(queryURL)
-.then(function (response) {
-    if (response.ok) {
-        return response.json();
-    } else {
-        throw new Error("Error: " + response.status);
-    }
-})
+    fetch(queryURL)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("Error: " + response.status);
+            }
+        })
 
 
-.then(function(data) {
-    //retrives the desired weather information from openWeather
-    var temperature = data.main.tempp;
-    var uvIndex = data.value.uvIndex;
-    var windSpeed = data.wind.speed;
-    var humidity = data.main.humidity;
+        .then(function (data) {
+            console.log(data)
+            //retrives the desired weather information from openWeather
+            // var temperature = data.main.tempp;
+            // var uvIndex = data.value.uvIndex;
+            // var windSpeed = data.wind.speed;
+            // var humidity = data.main.humidity;
 
-    //create dynamic html elements with weather data
-    cityName.text(city);
-    currentTemperature.text(temperature);
-    currentUvIndex.text(uvIndex);
-    currentWindSpeed.text(windSpeed);
-    currentHumidity.text(hummidity);
-})
-.catch(function (error) {
-    //handle error case
-    console.log("error: " + error.message);
-});
+            // //create dynamic html elements with weather data
+            // cityName.text(city);
+            // currentTemperature.text(temperature);
+            // currentUvIndex.text(uvIndex);
+            // currentWindSpeed.text(windSpeed);
+            // currentHumidity.text(hymidity);
+        })
+        .catch(function (error) {
+            //handle error case
+            console.log("error: " + error.message);
+        });
 }
 
 //function created for city search
 function searchCity() {
-    var city = citySearch.val();
+    var city = citySearch.value;
+    console.log(city)
     getWeatherData(city);
 
     //then stores searched city into local storage
-    sCity.push(city);
-    localStorage.setItem('cities', JSON.stringify(sCity));
+    // sCity.push(city);
+    // localStorage.setItem('cities', JSON.stringify(sCity));
 
-    //adds searched city to the city ist
-    var listItem = $("<li>").text(city);
-    cityList.append(listItem);
+    // //adds searched city to the city ist
+    // var listItem = document.createElement("li").innerText = city;
+    // cityList.append(listItem);
 
     //clears search field
-    citySearch.val('');
-} 
+    citySearch.value = '';
+}
 
 //retrive and display stored cities from the local storage
-$(document).ready(function () {
+function renderPreviousSearch() {
     sCity.forEach(function (city) {
         var listItem = $("<li>").text(city);
         cityList.append(listItem);
@@ -83,9 +85,9 @@ $(document).ready(function () {
 
     //get the weather data for the fist city in the list
     if (sCity.length > 0) {
-        getWeatherData(sCity[0]);
+       // getWeatherData(sCity[0]);
     }
-});
+};
 
 
 
